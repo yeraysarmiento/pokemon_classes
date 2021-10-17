b795cf6ca4211922c93c0f56172ac266ed1c5344
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-17 21:19:50 +0200

    Deleted form.html

--- build/app/Form/Form.js
+++ build/app/Form/Form.js
@@ -10,7 +10,27 @@ class Form extends Component {
   }
 
   generateHTML() {
-    const formHTML = ``;
+    const formHTML = `<form class="pokemon-card" action="ejemplo.php" method="get">
+      <div class="form-container">
+        <h3>YOUR POKEMON!</h3>
+        <p>NAME:</p>
+        <input type="text" name="pokemonName">
+        <p>IMAGE URL:</p>
+        <input type="url" name="pokemonImage"></input>
+        <p>TYPE:</p>
+        <select name="menu">
+            <option value="0"></option>
+            <option value="1">Water</option>
+            <option value="2">Grass</option>
+            <option value="3">Fire</option>
+            <option value="3">Electric</option>
+        </select>
+      </div>
+        <p>
+            <input type="reset" value="">
+            <input type="submit" value="">
+        </p>
+    </form>`;
 
     this.element.innerHTML = formHTML;
   }
