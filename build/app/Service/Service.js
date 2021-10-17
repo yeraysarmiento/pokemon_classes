af3081cfad1e5539e0d38ab8e923d8de08bd7a57
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-17 17:29:45 +0200

    Add button selected effect to navigator in Pokedex

--- build/app/Service/Service.js
+++ build/app/Service/Service.js
@@ -22,20 +22,14 @@ class Service {
     response = await response.json();
     return response;
   }
+
+  async deleteData(id) {
+    fetch(`${this.urlAPI}${id}`, { method: "DELETE" });
+    if (response.ok) {
+      return true;
+    }
+    throw new Error("Error bla bla");
+  }
 }
 
 export default Service;
-
-/*
-async createData(pokemon) {
-    const response = await fetch(this.urlAPI, {
-      method: "POST",
-      body: JSON.stringify(pokemon),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const newPokemon = response.json();
-    return newPokemon;
-  }
-  */
