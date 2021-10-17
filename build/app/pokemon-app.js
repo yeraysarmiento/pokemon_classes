65a95dedbd5a628321b6c637191a42473bc80b46
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-17 17:53:07 +0200

    Deteled pokedex.html

--- build/app/pokemon-app.js
+++ build/app/pokemon-app.js
@@ -4,3 +4,11 @@ const appContainer = document.querySelector(".app");
 const myUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=50";
 
 new Page(appContainer, myUrl);
+
+const pika = {
+  name: "bulbasaur",
+  url: "https://pokeapi.co/api/v2/pokemon/1/",
+};
+
+// const hola = new Service(myUrl);
+// hola.createData(pika);
